89cab2946f2ee25c5d88188a29f041e9ed3d97ef
Maxim <52193360+maxim201977@users.noreply.github.com>
2020-12-25 11:24:33 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Cookies_Hack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Copy Files";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(94, 13);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(116, 79);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 110);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(197, 14);
+            this.progressBar1.Step = 50;
+            this.progressBar1.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(217, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(147, 111);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(376, 136);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cookies Hack";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Cookies_Hack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //string[] browsers_list = {"Google Chrome", "Mozilla Firefox", "Microsoft Edge", "Opera", "Internet Explorer"};
+            string[] browsers_list = { "Google Chrome" };
+            foreach (string name in browsers_list) {
+                checkedListBox1.Items.Add(name);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedItems.Count != 0)
+            {
+                progressBar1.Value = 0;
+                richTextBox1.Clear();
+
+                button1.Enabled = false;
+                progressBar1.Step = 25;
+                progressBar1.PerformStep();
+                string user_name = Environment.UserName;
+                outToLog("Search coockies for user " + user_name);
+                progressBar1.PerformStep();
+
+                for (int x = 0; x < checkedListBox1.CheckedItems.Count; x++)
+                {
+                    Console.WriteLine(checkedListBox1.CheckedItems[x].ToString());
+                    switch (checkedListBox1.CheckedItems[x].ToString())
+                    {
+                        case "Google Chrome":
+                            string path_to_coocki = @"C:\Users\" + user_name + @"\AppData\Local\Google\Chrome\User Data\Default\Cookies";
+                            if (File.Exists(path_to_coocki))
+                            {
+                                outToLog("Сookies found");
+                                progressBar1.PerformStep();
+                                outToLog("Start coping");
+                                string exe_path = Environment.CurrentDirectory;
+                                if (File.Exists(exe_path + @"\" + Path.GetFileName(path_to_coocki))) {
+                                    File.Delete(exe_path + @"\" + Path.GetFileName(path_to_coocki));
+                                    File.Copy(path_to_coocki, exe_path + @"\" + Path.GetFileName(path_to_coocki));
+                                }
+                                else
+                                {
+                                    File.Copy(path_to_coocki, exe_path + @"\" + Path.GetFileName(path_to_coocki));
+                                }
+                                outToLog("Coping successful");
+                                progressBar1.PerformStep();
+                                button1.Enabled = true;
+                            }
+                            else
+                            {
+                                outToLog("No cookies found");
+                                progressBar1.Value = 0;
+                            }
+                            break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Select browser", "Erorr", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1.Enabled = true;
+            }
+
+        }
+
+        void outToLog(string output)
+        {
+            richTextBox1.AppendText(output + "\r\n");
+            richTextBox1.ScrollToCaret();
+        }
+    }
+}
